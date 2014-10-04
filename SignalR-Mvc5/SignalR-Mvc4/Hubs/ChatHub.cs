6221ddc204406bcaedb06bd083354d8f4e2c04ae
smyk <k.smykala@gmail.com>
2014-10-04 20:35:09 +0200

    Simple chat with login

---
+++ SignalR-Mvc5/SignalR-Mvc4/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalR_Mvc4.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string message)
+        {
+            var name = Context.User.Identity.Name;
+            Clients.All.addNewMessage(name, message);
+        }
+    }
+}
